11a3c779a4cd1ae91170e9989970699aaa440329
Julien Schueller <schueller@phimeca.com>
2018-10-12 13:42:18 +0200

    Add README

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# build parent doc
 make -C parent/doc html
-cp ./parent/doc/_build/html/objects.inv ./child/doc/foo-objects.txt
-make -C child/doc html
 
-# firefox ./child/doc/_build/html/index.html
+# copy sphinx inventory
+cp ./parent/doc/_build/html/objects.inv ./child/doc/foo-objects.txt
+
+# build child doc
+make -C child/doc html
